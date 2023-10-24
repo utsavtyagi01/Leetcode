b8406b73da55f07a5ebce725a68d26997d9a28b2
UTSAV <122996762+utsavtyagi01@users.noreply.github.com>
2023-10-24 17:35:53 +0530

    Create Remove_Element.cpp

---
+++ Remove_Element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+  int removeElement(vector<int>& nums, int val) {
+      int i=0;
+      int j=nums.size()-1;
+
+      while(i<=j){
+          if(nums[i]==val){
+              swap(nums[i],nums[j]);
+              j--;
+          }
+          else{
+              i++;
+          }
+      }
+      return i;
+  }
+};
