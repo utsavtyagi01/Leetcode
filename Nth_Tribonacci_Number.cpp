aa889be4f6d199cb4aa265a577180d22dd410e88
UTSAV <122996762+utsavtyagi01@users.noreply.github.com>
2024-04-24 20:37:43 +0530

    Create Nth_Tribonacci_Number.cpp

---
+++ Nth_Tribonacci_Number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+   
+    int tribonacci(int n) {
+        if(n==0)
+        return 0;
+
+        if(n==1 || n==2)
+        return 1;
+        int t1=0,t2=1,t3=1,result=0;
+        for(int i=3;i<=n;i++){
+            result=t1+t2+t3;
+            t1=t2;
+            t2=t3;
+            t3=result;
+        }
+        return result;
+    }
+};
