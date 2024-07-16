13fb80030f4863ee0759199ca499631fcdacc74d
UTSAV <122996762+utsavtyagi01@users.noreply.github.com>
2024-07-16 13:06:35 +0530

    Create Lowest_Common_Ancestor_Of_A_Binary_Tree.cpp

---
+++ Lowest_Common_Ancestor_Of_A_Binary_Tree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lca(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(root==NULL || root==p || root == q )
+        return root;
+
+        TreeNode* left=lca(root->left,p,q);
+        TreeNode* right=lca(root->right,p,q);
+
+        if(left && right)
+        return root;
+
+        if(left)
+        return left;
+
+        return right;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return lca(root,p,q);
+    }
+};
